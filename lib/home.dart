003694fcda2335a0c2dad992de00e29deaf62a69
Ajay <ajayvelhal@gmail.com>
2019-05-20 13:08:22 +0530

    Done Card

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,41 @@
-
 import 'package:flutter/material.dart';
 
 import 'app_bar.dart';
 
-class MyHome extends StatelessWidget{
+class MyHome extends StatelessWidget {
   final String title;
 
   MyHome(this.title);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      body: new MyBar(),
-    );
-  }
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Expanded(child: new MyBar(),),
 
-}
+        new Expanded(
+          child: ListView.builder(itemCount:2,itemBuilder: (BuildContext context,int index) {
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    child: Image.asset("assets/Layer 148.png"),
+                  ),
+                  Card(
+                    child: Image.asset("assets/Layer 149.png"),
+                  ),
+                  Card(
+                    child: Image.asset("assets/Layer 150.png"),
+                  )
+                ],
+              ),
+            );
+          }),
+        )
+    ]
+    );}
+
+  }
